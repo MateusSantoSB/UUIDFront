6677206be9b681d5db21fc6e5d8a1f9d75e38712
Tatetinho <141674380+MateusSantoSB@users.noreply.github.com>
2025-01-24 10:42:02 -0300

    servico url api atualizada

--- src/servico/service-generate-uuid.service.ts
+++ src/servico/service-generate-uuid.service.ts
@@ -11,7 +11,7 @@ export class ServiceGenerateUUIDService {
 
 
 generate():Observable<Generator>{
-    return this.http.get<Generator>("https://uuidgenerator1-0.onrender.comgit/generator/generateUUID")
+    return this.http.get<Generator>("https://uuidgenerator1-0.onrender.com/generator/generateUUID")
 }
 
 }
